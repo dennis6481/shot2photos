752bc0ea5a9ad8cd6731bcdc3b2dfa2d64c14d59
Rui MA <dennismachn@outlook.com>
2026-09-16 13:48:07 +0200

    Initial Commit

---
+++ Shot2Photos/Shot2PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shot2PhotosApp.swift
+//  Shot2Photos
+//
+//  Created by Rui Ma on 16/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Shot2PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
